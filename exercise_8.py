d3fc263251fd3b3dc59e8d4a85a1c63b3b9b586b
kirill965 <65949453+kirill965@users.noreply.github.com>
2021-10-27 10:42:51 +0200

    Add files via upload

---
+++ exercise_8.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Print on the screen actual date and time."""
+
+#importing time module
+import time
+
+#print on the screen actual date and time
+print(f'Actual date: {time.asctime()}.')
